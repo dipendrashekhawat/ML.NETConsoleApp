e1945474a86e0fedee74332fd8718d1c5d7afc21
Dipendra Shekhawat <dipendra.shekhawat@AASPL-BRD.COM>
2019-12-06 19:04:49 +0530

    initial setup for ML.NET console app

---
+++ MLConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace MLConsoleApp
+{
+    class Feature
+    {
+        string FeatureText;
+        bool label;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to ML World!");
+        }
+    }
+}
